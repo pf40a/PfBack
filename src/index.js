d53516d2d2cf2cd7daffda9c38c5293204e3e7a4
“rcosme85” <rcosme85@hotmail.com>
2023-08-31 16:23:12 -0500

    Actualizacion de db

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const sequelize = require("./db");
 const router = require("./Router/mainRouter");
 const port = 3001;
 const app = express();
-
+const { PORT } = process.env;
 const { Usuarios, Reviews, Reservas } = require("./Models/Relations");
 
 // MIDDLEWARES
@@ -22,7 +22,7 @@ app.use("/hotel", router);
 sequelize
   .sync({ force: true })
   .then(() => {
-    app.listen(port, () => {
+    app.listen(PORT, () => {
       console.log("Server on PORT :" + port);
     });
   })
