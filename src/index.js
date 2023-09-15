7ac0b10a863c3e209ddd123292af04b205cd0473
“rcosme85” <rcosme85@hotmail.com>
2023-09-15 17:03:04 -0500

    Actualización en carga de Imagenes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
 const sequelize = require("./db");
 const router = require("./Router/mainRouter");
 const port = 3001;
@@ -14,6 +16,13 @@ const { Usuarios, Reviews, Reservas } = require("./Models/Relations");
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 // MAIN ROUTER
 app.use("/hotel", router);
